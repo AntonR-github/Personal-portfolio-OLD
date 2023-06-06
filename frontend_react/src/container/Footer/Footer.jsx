2e327754c7d5a03adfd2285bb9facd8e0e0724e0
Anton <99226421+AntonR-github@users.noreply.github.com>
2023-06-06 21:26:11 +0300

    Add files via upload

--- frontend_react/src/container/Footer/Footer.jsx
+++ frontend_react/src/container/Footer/Footer.jsx
@@ -42,7 +42,7 @@ const Footer = () => {
       <div className="app__footer-cards">
         <div className="app__footer-card ">
           <img src={images.email} alt="email" />
-          <a href="mailto:antonrwk@gmail.com" className="p-text">antonrwk@gmail.com</a>
+          <a href="mailto:antonrwk@gmail.com" className="p-text">antonr1wk@gmail.com</a>
         </div>
         <div className="app__footer-card">
           <img src={images.mobile} alt="phone" />
